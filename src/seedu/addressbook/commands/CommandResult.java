a0d8c884e5737c474ef8bd416786cb4573e0fc58
Shannon <shannonyongjs@gmail.com>
2017-01-29 17:51:01 +0800

    Add setter and getter for feedbackToUser and made it private

--- src/seedu/addressbook/commands/CommandResult.java
+++ src/seedu/addressbook/commands/CommandResult.java
@@ -11,18 +11,18 @@ import java.util.Optional;
 public class CommandResult {
 
     /** The feedback message to be shown to the user. Contains a description of the execution result */
-    public final String feedbackToUser;
+    private String feedbackToUser;
 
     /** The list of persons that was produced by the command */
     private final List<? extends ReadOnlyPerson> relevantPersons;
 
     public CommandResult(String feedbackToUser) {
-        this.feedbackToUser = feedbackToUser;
+        this.setFeedbackToUser(feedbackToUser);
         relevantPersons = null;
     }
 
     public CommandResult(String feedbackToUser, List<? extends ReadOnlyPerson> relevantPersons) {
-        this.feedbackToUser = feedbackToUser;
+        this.setFeedbackToUser(feedbackToUser);
         this.relevantPersons = relevantPersons;
     }
 
@@ -31,6 +31,14 @@ public class CommandResult {
      */
     public Optional<List<? extends ReadOnlyPerson>> getRelevantPersons() {
         return Optional.ofNullable(relevantPersons);
+    }
+    
+    public String getFeedbackToUser(){
+    	return feedbackToUser;
+    }
+    
+    private void setFeedbackToUser(String feedback){
+    	feedbackToUser = feedback;
     }
 
 }
