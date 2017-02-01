8632337a057c25d538b3f6d4921e41221676b487
Shannon <shannonyongjs@gmail.com>
2017-02-01 20:27:14 +0800

    add methods to Formatter

--- src/seedu/addressbook/ui/Formatter.java
+++ src/seedu/addressbook/ui/Formatter.java
@@ -1,4 +1,16 @@
 package seedu.addressbook.ui;
+import static seedu.addressbook.common.Messages.*;
+import static seedu.addressbook.ui.Formatter.DISPLAYED_INDEX_OFFSET;
+import static seedu.addressbook.ui.Formatter.DIVIDER;
+import static seedu.addressbook.ui.Formatter.MESSAGE_INDEXED_LIST_ITEM;
+import static seedu.addressbook.ui.Formatter.getIndexedListItem;
+import seedu.addressbook.commands.CommandResult;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import seedu.addressbook.data.person.ReadOnlyPerson;
 
 public class Formatter {
 	
@@ -19,4 +31,47 @@ public class Formatter {
 
     /** Format of a comment input line. Comment lines are silently consumed when reading user input. */
     public static final String COMMENT_LINE_FORMAT_REGEX = "#.*";
+    
+    
+    /**
+     * Formats a string as a viewable indexed list item.
+     *
+     * @param visibleIndex visible index for this listing
+     */
+    public static String getIndexedListItem(int visibleIndex, String listItem) {
+        return String.format(MESSAGE_INDEXED_LIST_ITEM, visibleIndex, listItem);
+    }
+    
+    /** Formats a list of strings as a viewable indexed list. */
+    public static String getIndexedListForViewing(List<String> listItems) {
+        final StringBuilder formatted = new StringBuilder();
+        int displayIndex = 0 + DISPLAYED_INDEX_OFFSET;
+        for (String listItem : listItems) {
+            formatted.append(getIndexedListItem(displayIndex, listItem)).append("\n");
+            displayIndex++;
+        }
+        return formatted.toString();
+    }
+    
+    public String getResultToShowToUser(CommandResult result){
+    	 final Optional<List<? extends ReadOnlyPerson>> resultPersons = result.getRelevantPersons();
+         if (resultPersons.isPresent()) {
+             getShowPersonListView(resultPersons.get());
+         }
+         return result.feedbackToUser + DIVIDER;
+    }
+    
+    /**
+     * Shows a list of persons to the user, formatted as an indexed list.
+     * Private contact details are hidden.
+     */
+    private String getShowPersonListView(List<? extends ReadOnlyPerson> persons) {
+        final List<String> formattedPersons = new ArrayList<>();
+        for (ReadOnlyPerson person : persons) {
+            formattedPersons.add(person.getAsTextHidePrivate());
+        }
+        return getIndexedListForViewing(formattedPersons);
+        
+    }
+
 }
--- src/seedu/addressbook/ui/TextUi.java
+++ src/seedu/addressbook/ui/TextUi.java
@@ -117,7 +117,7 @@ public class TextUi {
      * Private contact details are hidden.
      */
     private void showPersonListView(List<? extends ReadOnlyPerson> persons) {
-        final List<String> formattedPersons = new ArrayList<>();
+    	final List<String> formattedPersons = new ArrayList<>();
         for (ReadOnlyPerson person : persons) {
             formattedPersons.add(person.getAsTextHidePrivate());
         }
@@ -126,27 +126,12 @@ public class TextUi {
 
     /** Shows a list of strings to the user, formatted as an indexed list. */
     private void showToUserAsIndexedList(List<String> list) {
-        showToUser(getIndexedListForViewing(list));
+        showToUser(Formatter.getIndexedListForViewing(list));
     }
 
-    /** Formats a list of strings as a viewable indexed list. */
-    private static String getIndexedListForViewing(List<String> listItems) {
-        final StringBuilder formatted = new StringBuilder();
-        int displayIndex = 0 + DISPLAYED_INDEX_OFFSET;
-        for (String listItem : listItems) {
-            formatted.append(getIndexedListItem(displayIndex, listItem)).append("\n");
-            displayIndex++;
-        }
-        return formatted.toString();
-    }
 
-    /**
-     * Formats a string as a viewable indexed list item.
-     *
-     * @param visibleIndex visible index for this listing
-     */
-    private static String getIndexedListItem(int visibleIndex, String listItem) {
-        return String.format(MESSAGE_INDEXED_LIST_ITEM, visibleIndex, listItem);
-    }
+
+
+
 
 }
